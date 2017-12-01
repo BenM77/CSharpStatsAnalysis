f608e5d97353f2c7ac473fe47be6eba434e5a828
droberts18 <droberts18@LAB-SCI-214-07>
2017-12-01 12:57:56 -0800

    Create seq functions

--- CSharpStatsAnalysis/CSharpStatsAnalysis/Program.cs
+++ CSharpStatsAnalysis/CSharpStatsAnalysis/Program.cs
@@ -8,10 +8,45 @@ namespace CSharpStatsAnalysis
 {
     class Program
     {
+        static double[] seqLength(int start, int end, int length)
+        {
+            double[] seq = new double[length];
+
+            int diff = end - start;
+            length--;
+
+            double increment = diff / (double)length;
+
+            int index = 0;
+            for(double i = start; i <= end; i += increment)
+            {
+                seq[index] = i;
+                index++;
+            }
+
+            return seq;
+        }
+
+        static int[] seqBy(int start, int end, int by)
+        {
+            int numArrElements = (end - start) / by;
+            int[] seq = new int[numArrElements+1];
+
+            int index = 0;
+            for(int i = start; i <= end; i += by)
+            {
+                seq[index] = i;
+                index++;
+            }
+
+            return seq;
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("hello world");
-            Console.Write("test");
+            int[] test = seqBy(1, -10, 5);
+            for(int i = 0; i < test.Length; i++)
+                Console.WriteLine(test[i]);
         }
     }
 }
