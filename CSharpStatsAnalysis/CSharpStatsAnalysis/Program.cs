d2abb5794adebf1fa48c7f6e5a5011104e38ca26
droberts18 <droberts18@LAB-SCI-214-07>
2017-12-15 02:28:19 -0800

    Add Main code

--- CSharpStatsAnalysis/CSharpStatsAnalysis/Program.cs
+++ CSharpStatsAnalysis/CSharpStatsAnalysis/Program.cs
@@ -1,21 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-namespace CSharpStatsAnalysis
+﻿namespace CSharpStatsAnalysis
 {
     class Program
     {
         static void Main(string[] args)
         {
             NumberVector nv = new NumberVector(1, 10, false);
-            nv.getMedian();
-            nv.getLowerQuartile();
-            nv.getUpperQuartile();
-            nv.displayStats();
+            NumberVector nv2 = new NumberVector(2, 6, 2);
+            Matrix m = new Matrix(nv, nv2, true);
+
+            double[] arr = new double[] { 1, 5, 8, 1, 3, 4, 9, 10, 2, 4, 4, 5, 6, 7, 3, 4, 5, 10, 9, 6 };
+            NumberVector nv3 = new NumberVector(arr);
+            Matrix m2 = new Matrix(nv3, 2, 10);
+
+            NumberVector nv4 = m.convertToVector();
+            nv4.displayStats();
         }
     }
 }
